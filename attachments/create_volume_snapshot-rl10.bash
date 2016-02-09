1ac3cadba29a8fae58f74c079084bdffd9db29b3
Chris Fordham <chris@fordham-nagy.id.au>
2016-02-09 12:09:15 +1100

    Add create_volume_snapshot-rl10.bash.

---
+++ attachments/create_volume_snapshot-rl10.bash
@@ -0,0 +1,54 @@
+#! /bin/bash -e
+
+if [ -z "$1" ]; then
+  echo "Please provide a volume lineage name to snapshot."
+  exit 1
+fi
+
+volfound=0
+lineage="$1"
+
+echo "Querying instance self..."
+instance_href=$(sudo /usr/local/bin/rsc --rl10 cm15 --x1 ':has(.rel:val("self")).href' index_instance_session /api/sessions/instance)
+server_href=$(sudo /usr/local/bin/rsc --rl10 --pp cm15 show --x1 ':has(.rel:val("parent")).href' "$instance_href")
+cloud_href=$(sudo /usr/local/bin/rsc --rl10 cm15 show "$instance_href" view=extended --x1 ':has(.rel:val("cloud")).href')
+datacenter_href=$(sudo /usr/local/bin/rsc --rl10 cm15 show "$instance_href" view=extended --x1 ':has(.rel:val("datacenter")).href')
+echo "cloud href:      $cloud_href"
+echo "instance href:   $instance_href"
+echo "server href:     $server_href"
+echo "datacenter href: $datacenter_href"
+
+cloud_id=$(basename "$cloud_href")
+
+volumes=$(sudo /usr/local/bin/rsc --rl10 --pp cm15 index --xm ".href" "$instance_href/volume_attachments" | grep "/volumes/" | tr -d '"')
+
+mapfile -t vols <<< "$volumes"
+
+for v in "${vols[@]}"
+do
+  echo "Looking up $v"
+  vol=$(sudo /usr/local/bin/rsc --rl10 --pp cm15 show "$v")
+  name=$(echo "$vol" | jq --raw-output .name)
+  if [ "$name" = "$lineage" ]; then
+    volfound=1
+    href="$v"
+    break
+  fi
+done
+
+if [ "$volfound" -eq 1 ]; then
+  echo 'Volume found.'
+  echo "$vol"
+  echo 'Taking snapshot.'
+  sudo /usr/local/bin/rsc --rl10 cm15 create "/api/clouds/$cloud_id/volume_snapshots" \
+    "volume_snapshot[name]=$lineage-$(date +%Y%m%d%H%M%S)" \
+    "volume_snapshot[description]=Snapshot taken on $(hostname -f)" \
+    "volume_snapshot[parent_volume_href]=$href"
+else
+  echo "No volume found attached to instance with name, '$lineage', exiting."
+  exit 1
+fi
+
+echo 'Snapshot creation complete.'
+
+# end
