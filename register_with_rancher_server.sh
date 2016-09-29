5edc879231a2aa8f61d93096bb8cf84b1ab6b9a5
Chris Fordham <chris@fordham-nagy.id.au>
2016-09-29 12:16:30 +1000

    Add support for providing a RANCHER_HOST_EXTERNAL_DNS_IP.

--- register_with_rancher_server.sh
+++ register_with_rancher_server.sh
@@ -7,6 +7,8 @@
 # $CATTLE_HOST_LABELS
 # $RANCHER_AGENT_TAG
 
+: "${RANCHER_HOST_EXTERNAL_DNS_IP:=}"
+
 # https://github.com/rancher/rancher/issues/1370
 # Note that due this bug you may need to action the following if
 # receiving 401 Unauthorized we a previously registered host:
@@ -22,6 +24,11 @@ private_ip=`ifconfig $iface | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep
 
 env=$(sudo cat /var/spool/rancher/registration.sh)
 eval "$env"
+
+# pre-add in the external DNS label if provided
+if [ ! -z "$RANCHER_HOST_EXTERNAL_DNS_IP" ]; then
+  CATTLE_HOST_LABELS="$CATTLE_HOST_LABELS&io.rancher.host.external_dns_ip=$RANCHER_HOST_EXTERNAL_DNS_IP"
+fi
 
 if [ ! -z "$CATTLE_HOST_LABELS" ]; then
   labels=(${CATTLE_HOST_LABELS//&/ })
