f088feb6a9c18286338938838c272911a8406817
Chris Fordham <chris@fordham-nagy.id.au>
2016-11-01 22:31:12 +1100

    Initial support for post commands in container, ssh priv key injection.

--- install_and_run_rancher.bash
+++ install_and_run_rancher.bash
@@ -10,6 +10,8 @@
 : "${RANCHER_DOCKER_OPTS:=}"
 : "${RANCHER_DOCKER_CMD:=}"
 : "${RANCHER_TAG:=stable}"
+: "${RANCHER_CONTAINER_POST_CMDS:=}"
+: "${RANCHER_CONTAINER_SSH_PRIV_KEY}"
 
 [ ! -z "$RANCHER_CONTAINER_TIMEZONE" ] && RANCHER_DOCKER_OPTS="$RANCHER_DOCKER_OPTS -e TZ=$RANCHER_CONTAINER_TIMEZONE"
 
@@ -30,7 +32,7 @@ function run_rancher_with_external_db()
     -e CATTLE_DB_CATTLE_MYSQL_NAME=$CATTLE_DB_CATTLE_MYSQL_NAME \
     -e CATTLE_DB_CATTLE_USERNAME=$CATTLE_DB_CATTLE_USERNAME \
     -e CATTLE_DB_CATTLE_PASSWORD=$CATTLE_DB_CATTLE_PASSWORD \
-    "rancher/server:$RANCHER_TAG" "$RANCHER_DOCKER_CMD"
+    "rancher/server:$RANCHER_TAG" $RANCHER_DOCKER_CMD
 }
 
 function run_rancher_all_in_one()
@@ -45,6 +47,42 @@ function run_rancher()
     else
         run_rancher_all_in_one
     fi
+}
+
+function container_id()
+{
+  local container_id=$(docker inspect --format="{{.Id}}" "$(docker ps | grep 'rancher/server' | awk '{print $NF}')")
+  echo "$container_id"
+}
+
+# the use of `exec` is not preferrable
+# currently a workaround as rightlink as no tty needed for some docker commands
+
+function inject_ssh_priv_key()
+{
+  echo 'Inject SSH key.'
+  local tmpfile="$(mktemp /tmp/rs.XXXXXXXXXX)"
+  echo "$RANCHER_CONTAINER_SSH_PRIV_KEY" > "$tmpfile"
+  # [[ $- == *i* ]] && echo 'Interactive' || echo 'Not interactive'
+  cid="$(container_id)"
+  sudo docker exec "$cid" mkdir -p /root/.ssh
+  echo "$(exec sudo docker cp $tmpfile $cid:/root/.ssh/id_rsa)"
+  sleep 5
+  rm -f "$tmpfile"
+  sudo docker exec "$cid" chmod 400 /root/.ssh/id_rsa
+}
+
+function run_post_cmds()
+{
+  echo 'Running post commands within container...'
+  cid="$(container_id)"
+  local tmpfile="$(mktemp /tmp/rs.XXXXXXXXXX)"
+  echo '#!/bin/bash -e' > "$tmpfile"
+  echo "$RANCHER_CONTAINER_POST_CMDS" >> "$tmpfile"
+  echo "$(exec sudo docker cp $tmpfile $cid:/tmp/rancher-post.bash)"
+  sudo docker exec "$cid" chmod +x /tmp/rancher-post.bash
+  sudo docker exec "$cid" /tmp/rancher-post.bash
+  sudo docker exec "$cid" rm /tmp/rancher-post.bash
 }
 
 #---------------------------------------------------------------------------------------------------------------------------
@@ -63,5 +101,9 @@ if ! sudo docker ps | grep 'rancher/server'; then
 else
   echo 'rancher/server already running, skipping.'
 fi
+
+[ ! -z "$RANCHER_CONTAINER_SSH_PRIV_KEY" ] && inject_ssh_priv_key
+
+[ ! -z "$RANCHER_CONTAINER_POST_CMDS" ] && run_post_cmds
 
 echo 'Done.'
