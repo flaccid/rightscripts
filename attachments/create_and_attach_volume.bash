6f3d5e20a1f54d1d25fd197474ea9445b5fb8ac8
Chris Fordham <chris@fordham-nagy.id.au>
2016-11-16 15:02:12 +1100

    Default to /dev/sdc, minor syntax fixes, error handling for rsc requests, support AzureRM.

--- attachments/create_and_attach_volume.bash
+++ attachments/create_and_attach_volume.bash
@@ -1,8 +1,8 @@
 #! /bin/bash -e
 
-: "${DOCKER_DIR_BLOCK_DEVICE:=/dev/sdb}"
+: "${DOCKER_DIR_BLOCK_DEVICE:=/dev/sdc}"
 : "${DOCKER_DIR_VOLUME_SIZE:=10}"
-: "${DOCKER_DIR_VOLUME_NAME:=`hostname`-dockerdata}"
+: "${DOCKER_DIR_VOLUME_NAME:=$(hostname)-dockerdata}"
 : "${DOCKER_DIR_VOLUME_TYPE_HREF:=}"
 : "${DOCKER_DIR_VOLUME_SNAPSHOT_HREF:=}"
 : "${DOCKER_DIR_SAS_DEVICE:=}"
@@ -16,26 +16,38 @@ echo "Querying instance self..."
 instance_href=$(sudo /usr/local/bin/rsc --rl10 cm15 --x1 ':has(.rel:val("self")).href' index_instance_session /api/sessions/instance)
 server_href=$(sudo /usr/local/bin/rsc --rl10 --pp cm15 show --x1 ':has(.rel:val("parent")).href' "$instance_href")
 cloud_href=$(sudo /usr/local/bin/rsc --rl10 cm15 show "$instance_href" view=extended --x1 ':has(.rel:val("cloud")).href')
-datacenter_href=$(sudo /usr/local/bin/rsc --rl10 cm15 show "$instance_href" view=extended --x1 ':has(.rel:val("datacenter")).href')
+# not all clouds have 'datacenters'
+datacenter_href=$(sudo /usr/local/bin/rsc --rl10 cm15 show "$instance_href" view=extended --x1 ':has(.rel:val("datacenter")).href' 2>/dev/null || true)
+placement_group_href=$(sudo /usr/local/bin/rsc --rl10 cm15 show "$instance_href" view=extended --x1 ':has(.rel:val("placement_group")).href')
+cloud_type=$(sudo /usr/local/bin/rsc --rl10 cm15 show "$cloud_href" --x1 ".cloud_type")
 echo "cloud href:      $cloud_href"
 echo "instance href:   $instance_href"
 echo "server href:     $server_href"
 echo "datacenter href: $datacenter_href"
+echo "placement group href: $placement_group_href"
+echo "cloud type: $cloud_type"
+echo
 
-echo "Creating volume $DOCKER_DIR_VOLUME_SIZEGB volume..."
-cmd="sudo /usr/local/bin/rsc --rl10 cm15 create --dump=debug "$cloud_href/volumes" \
-  volume[name]=$DOCKER_DIR_VOLUME_NAME \
-  volume[size]=$DOCKER_DIR_VOLUME_SIZE \
-  volume[datacenter_href]=$datacenter_href"
+# construct the volume creation command
+# name and size are basically the only two common params between all clouds
+cmd="sudo /usr/local/bin/rsc --rl10 cm15 create --dump=debug $cloud_href/volumes \
+  'volume[name]=$DOCKER_DIR_VOLUME_NAME' \
+  'volume[size]=$DOCKER_DIR_VOLUME_SIZE'"
+
+# any other params not found to be empty, append to the command
+[ ! -z "$datacenter_href" ] && cmd="$cmd 'volume[datacenter_href]=$datacenter_href'"
+[ ! -z "$placement_group_href" ] && cmd="$cmd 'volume[placement_group_href]=$placement_group_href'"
 [ ! -z "$DOCKER_DIR_VOLUME_TYPE_HREF" ] && cmd="$cmd 'volume[volume_type_href]=$DOCKER_DIR_VOLUME_TYPE_HREF'"
 [ ! -z "$DOCKER_DIR_VOLUME_SNAPSHOT_HREF" ] && cmd="$cmd 'volume[parent_volume_snapshot_href]=$DOCKER_DIR_VOLUME_SNAPSHOT_HREF'"
 
-api_result=$(eval "$cmd" 2>&1)
+echo "Creating new ${DOCKER_DIR_VOLUME_SIZE}GB volume..."
+echo "> $cmd"
+api_result=$(eval "$cmd" 2>&1 || true)
 
 case "$api_result" in
   *"201 Created"*)
     volume_href=$(echo "$api_result" | grep 'Location:' | awk '/Location:/ { print $2 }' | tr -d '\r' | tr -d '\n')
-    echo " volume, '$DOCKER_DIR_VOLUME_NAME' successfully created ($volume_href)."
+    echo "Volume, '$DOCKER_DIR_VOLUME_NAME' successfully created ($volume_href)."
   ;;
   *)
     echo "Volume creation FAILED: $api_result"
@@ -46,11 +58,16 @@ esac
 # corner case for GCE which doesn’t like full device paths/names but only wants the short name
 [[ "$cloud_href" = '/api/clouds/2175' ]] && DOCKER_DIR_BLOCK_DEVICE=$(echo "$DOCKER_DIR_BLOCK_DEVICE" | awk -F'[=/]' '{print $3}')
 
+# corner case for Azure where it needs iscsi device instead of linux logical device name
+[[ "$cloud_type" == *"azure"* ]] && DOCKER_DIR_BLOCK_DEVICE='01'
+
 echo 'Attaching the volume...'
-api_result=$(sudo /usr/local/bin/rsc --rl10 cm15 create --dump=debug "$cloud_href/volume_attachments" \
-  volume_attachment[device]="$DOCKER_DIR_BLOCK_DEVICE" \
-  volume_attachment[instance_href]="$instance_href" \
-  volume_attachment[volume_href]="$volume_href" 2>&1)
+cmd="sudo /usr/local/bin/rsc --rl10 cm15 create --dump=debug $cloud_href/volume_attachments \
+  volume_attachment[device]=$DOCKER_DIR_BLOCK_DEVICE \
+  volume_attachment[instance_href]=$instance_href \
+  volume_attachment[volume_href]=$volume_href"
+echo "> $cmd"
+api_result=$(eval "$cmd" 2>&1 || true)
 
 case "$api_result" in
   *"201 Created"*)
