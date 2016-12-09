f46cf57d2e2c03c5f94905853fe7d2ea3691edfe
Chris Fordham <chris@fordham-nagy.id.au>
2016-12-09 21:13:03 +1100

    Add remove_lxcbr0.sh.

---
+++ remove_lxcbr0.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+
+! ip addr | grep lxcbr0 && echo 'lxcbr0 not active, skipping.' && exit 0
+
+sudo ip route del 10.0.3.0/24
+sudo ifconfig lxcbr0 down
+sudo brctl delbr lxcbr0
+
+echo 'Done.'
