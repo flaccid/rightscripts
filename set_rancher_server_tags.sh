f6ff2b9c95bb8432e09c4d66957da41c436423e5
Chris Fordham <chris@fordham-nagy.id.au>
2016-12-13 15:59:48 +1100

    Bash shebang and more tags.

--- set_rancher_server_tags.sh
+++ set_rancher_server_tags.sh
@@ -1,6 +1,8 @@
-#! /bin/sh -e
+#! /bin/bash -e
 
-: "${RANCHER_SERVER_ID:=default}"
+: "${RANCHER_SERVER_IDENTIFIER:=default}"
+: "${RANCHER_SERVER_ENDPOINT_HOST:=rancher.localdomain}"
+: "${RANCHER_SERVER_PROTO:=http}"
 
 . /var/run/rightlink/secret
 
@@ -10,8 +12,24 @@ if ! type curl > /dev/null 2>&1; then
   sudo apt-get -y install curl || sudo yum -y install curl
 fi
 
-logger -s -t RightScale "Setting rancher:server_id tag"
+logger -s -t RightScale "Setting rancher:server tag"
 no_proxy=127.0.0.1 curl -sS -X POST -H X-RLL-Secret:$RS_RLL_SECRET -g \
-  "http://127.0.0.1:$RS_RLL_PORT/api/tags/multi_add?resource_hrefs[]=$RS_SELF_HREF&tags[]=rancher:server_id=$RANCHER_SERVER_ID"
+  "http://127.0.0.1:$RS_RLL_PORT/api/tags/multi_add?resource_hrefs[]=$RS_SELF_HREF&tags[]=rancher:server=true"
+
+logger -s -t RightScale "Setting rancher:server_identifier tag"
+no_proxy=127.0.0.1 curl -sS -X POST -H X-RLL-Secret:$RS_RLL_SECRET -g \
+  "http://127.0.0.1:$RS_RLL_PORT/api/tags/multi_add?resource_hrefs[]=$RS_SELF_HREF&tags[]=rancher:server_identifier=$RANCHER_SERVER_IDENTIFIER"
+
+logger -s -t RightScale "Setting rancher:proto tag"
+no_proxy=127.0.0.1 curl -sS -X POST -H X-RLL-Secret:$RS_RLL_SECRET -g \
+  "http://127.0.0.1:$RS_RLL_PORT/api/tags/multi_add?resource_hrefs[]=$RS_SELF_HREF&tags[]=rancher:proto=$RANCHER_SERVER_PROTO"
+
+logger -s -t RightScale "Setting rancher:endpoint_host tag"
+no_proxy=127.0.0.1 curl -sS -X POST -H X-RLL-Secret:$RS_RLL_SECRET -g \
+  "http://127.0.0.1:$RS_RLL_PORT/api/tags/multi_add?resource_hrefs[]=$RS_SELF_HREF&tags[]=rancher:endpoint_host=$RANCHER_SERVER_ENDPOINT_HOST"
+
+logger -s -t RightScale "Setting rancher:api_version tag"
+no_proxy=127.0.0.1 curl -sS -X POST -H X-RLL-Secret:$RS_RLL_SECRET -g \
+  "http://127.0.0.1:$RS_RLL_PORT/api/tags/multi_add?resource_hrefs[]=$RS_SELF_HREF&tags[]=rancher:api_version=$RANCHER_API_VERSION"
 
 echo 'Done.'
