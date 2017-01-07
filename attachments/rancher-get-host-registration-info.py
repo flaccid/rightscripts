476c10a82353659afb80517334ef437fbc4f104d
Chris Fordham <chris@fordham-nagy.id.au>
2017-01-07 20:14:23 +1100

    Support rancher URLs with or without api version in href, select token based on project if provided.

--- attachments/rancher-get-host-registration-info.py
+++ attachments/rancher-get-host-registration-info.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python
 
 import os
+import os.path
 import cattle
 import requests
+import urlparse
+from urlparse import urlparse
 
 RANCHER_API_VERSION = 1
 
-client = cattle.Client(url=os.environ['RANCHER_URL'] + '/v' + str(RANCHER_API_VERSION),
+# in case user has already provided the api version in the URL or a more specific URL
+if '/v' not in os.environ['RANCHER_URL']:
+    RANCHER_URL = urlparse.urljoin(os.environ['RANCHER_URL'], '/v' + str(RANCHER_API_VERSION))
+else:
+    RANCHER_URL = os.environ['RANCHER_URL']
+
+print('RANCHER_URL: ' + RANCHER_URL)
+print('RANCHER_ACCESS_KEY: ' + os.environ['RANCHER_ACCESS_KEY'])
+
+client = cattle.Client(url=RANCHER_URL,
                        access_key=os.environ['RANCHER_ACCESS_KEY'],
                        secret_key=os.environ['RANCHER_SECRET_KEY'])
 
-registration_token = client.list_registrationToken()
-registration_url = registration_token[0]['registrationUrl']
+registration_tokens = client.list_registrationToken()
+
+# debug
+# print('Registration tokens returned: ' + str(registration_tokens))
+
+# when the URL includes a project ID, get the token for that project
+if 'projects' in RANCHER_URL:
+    [token for token in registration_tokens if token["accountId"] == os.path.split(urlparse(RANCHER_URL).path)[-1]][0]
+else:
+    token = registration_tokens[0]
+
+registration_url = token['registrationUrl']
 registration_file = '/var/spool/rancher/registration.sh'
 
 print('GET: ' + registration_url)
