8e9b4809d6ed37ee12581b2323bd248cdb658bf6
Chris Fordham <chris@fordham-nagy.id.au>
2017-02-01 19:03:56 +1100

    Add start_ngrok_tunnel.bash.

---
+++ start_ngrok_tunnel.bash
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+
+: "${NGROK_AUTH_TOKEN:=}"
+: "${NGROK_LOG_FILE:=/tmp/ngrok.log}"
+: "${NGROK_REGION:=us}"
+: "${NGROK_TUNNEL_PROTO:=tcp}"
+: "${NGROK_TUNNEL_PORT:=22}"
+
+[ -z "$NGROK_AUTH_TOKEN" ] && echo '$NGROK_AUTH_TOKEN not set, exiting!' && exit 1
+
+if ! type ngrok > /dev/null 2>&1; then
+  echo 'installing ngrok...'
+
+  if ! type unzip > /dev/null 2>&1; then
+    sudo apt-get -y install unzip || sudo yum -y install unzip
+  fi
+
+  cd /tmp
+  curl -Ss https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > ngrok.zip
+  unzip ngrok.zip
+  sudo mkdir -p /usr/local/bin
+  sudo mv ./ngrok /usr/local/bin/ngrok
+  sudo chmod +x /usr/local/bin/ngrok
+  rm ngrok.zip
+
+  # sane path
+  export PATH="$PATH:/usr/local/bin"
+
+  ngrok --version
+fi
+
+# kill any existing ngrok processes found
+if pgrep ngrok; then
+  pgrep ngrok | xargs kill
+fi
+
+echo 'starting ngrok'
+
+nohup ngrok "$NGROK_TUNNEL_PROTO" "$NGROK_TUNNEL_PORT" \
+  --authtoken "$NGROK_AUTH_TOKEN" \
+  --log "$NGROK_LOG_FILE" \
+  --region "$NGROK_REGION" \
+    > /dev/null &
+
+sleep 5
+
+echo 'last 50 lines of log:'
+tail -n 50 "$NGROK_LOG_FILE"
+
+ngrok_tunnels=$(curl -Ss http://127.0.0.1:4040/api/tunnels)
+
+echo "$ngrok_tunnels"
+
+echo 'Done.'
