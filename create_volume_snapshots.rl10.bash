d1805d63e94e6915a640c425a8f414cae768fd21
Chris Fordham <chris@fordham-nagy.id.au>
2017-07-01 09:18:19 +1000

    Fix attachment script name.

--- create_volume_snapshots.rl10.bash
+++ create_volume_snapshots.rl10.bash
@@ -9,7 +9,7 @@ lineages=(${VOLUME_SNAPSHOTTING_LINEAGES//,/ })
 
 for lineage in "${lineages[@]}"
 do
-  sudo bash -e "$RS_ATTACH_DIR/create_volume_snapshot-rl10.bash" "$lineage"
+  sudo bash -e "$RS_ATTACH_DIR/create_volume_snapshot.rl10.bash" "$lineage"
 done
 
 echo 'Done.'
